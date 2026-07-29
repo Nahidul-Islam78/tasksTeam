009da26e47684ece8b03e19fdf460695939576d7
Nahidul-Islam <mdnahid784055@gmail.com>
2026-07-29 12:59:17 +0600

    added trusted brand section

--- src/component/Trusted.jsx
+++ src/component/Trusted.jsx
@@ -1,11 +1,85 @@
 import React from 'react';
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import AmazonVector from './../assets/brands/amazon_vector.png'
+import Amazon from './../assets/brands/amazon.png'
+import Casio from './../assets/brands/casio.png'
+import Moonstar from './../assets/brands/moonstar.png'
+import Randstad from './../assets/brands/randstad.png'
+import Star from './../assets/brands/star.png'
+import StartPeople from './../assets/brands/start_people.png'
 
 const Trusted = () => {
   return (
-    <div>
-      <p className='text-3xl text center'>Trusted by modern teams</p>
-      <div>
-        
+    <div className="text-[#ffffff] ">
+      <p className="text-3xl text center ">Trusted by modern teams</p>
+      <div className=" py-5">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={10}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay]}
+          className="mySwiper "
+        >
+          <SwiperSlide>
+            <img
+              src={AmazonVector}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={Amazon}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={Casio}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={Moonstar}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={Randstad}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={Star}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={StartPeople}
+              className="brightness-0 invert opacity-80 hover:opacity-100 transition-opacity"
+              alt=""
+            />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from '../../component/Hero';
+import Trusted from '../../component/Trusted';
 
 
 const HomePage = () => {
@@ -7,6 +8,9 @@ const HomePage = () => {
     <div>
       <div>
         <Hero></Hero>
+      </div>
+      <div>
+        <Trusted></Trusted>
       </div>
     </div>
   );
