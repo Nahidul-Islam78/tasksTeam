60a5edc1644e96264c6687f5492097cdb1db7cfe
Nahidul-Islam <mdnahid784055@gmail.com>
2026-08-08 17:04:01 +0600

    added login functionality

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
+import useAuth from '../../hooks/useAuth';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
 
 const Login = () => {
+  const { loginUser } = useAuth();
+  const { handleSubmit, register } = useForm();
+  const navigate=useNavigate()
+
+  const handelLogin = (data) => {
+  
+    loginUser(data.email, data.password).then(data => {
+      console.log(data);
+      navigate('/dashboard')
+    }).catch(error=>{
+      console.log(error)
+    })
+    
+  }
   return (
     <div>
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <div className="card-body">
-          <fieldset className="fieldset">
-            <label className="label">Name</label>
-            <input type="text" className="input" placeholder="Name" />
+          <form onSubmit={handleSubmit(handelLogin)} className="fieldset">
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input {...register('email')} type="email" className="input" placeholder="Email" />
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input {...register('password')} type="password" className="input" placeholder="Password" />
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
             <button className="btn btn-neutral mt-4">Login</button>
-          </fieldset>
+          </form>
         </div>
       </div>
     </div>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { auth } from '../firebase/firebase.init';
 import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth/cordova';
 
 const AuthProvider = ({ children }) => {
   const [user,setUser]=useState(null)
   //user signup
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
+  }
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
   }
   //set observer
   useEffect(() => {
@@ -21,6 +25,7 @@ const AuthProvider = ({ children }) => {
 
   const data = {
     createUser,
+    loginUser,
     user
   }
   
